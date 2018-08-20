da84c0d9a1c3f5ce41db96b17f4706f0ac9cceb9
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-20 16:00:36 +0200

    Add express for routes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,43 @@
 const functions = require('firebase-functions');
 const httpStatus = require('http-status-codes')
+const express = require('express')
+const cors = require('cors')({origin: true})
 const mailer = require('./mailer')
 const util = require('./util')
 
+const app = express()
 const mailTransport = mailer.getMailer()
+
+app.use(cors)
+
+// routes
+app.get('/hello', (req, res) => {
+  return res.json({
+    data: {
+      message: 'Hello World'
+    }
+  })
+})
+
+app.post('/mail', (req, res) => {
+  const result = util.validateEmail(req.body)
+  if (result.error) {
+    return res
+      .status(httpStatus.BAD_REQUEST)
+      .send({
+        code: httpStatus.BAD_REQUEST,
+        message: httpStatus.getStatusText(httpStatus.BAD_REQUEST),
+        details:
+          result.error.details &&
+          result.error.details.map(err => {
+            return {
+              message: err.message,
+              param: err.path.join('.')
+            }
+          })
+      })
+  }
+})
 
 exports.hello = functions.https.onRequest((request, response) => {
   if (request.method !== 'GET') {
@@ -21,50 +55,4 @@ exports.hello = functions.https.onRequest((request, response) => {
   })
 });
 
-exports.mail = functions.https.onRequest((request, response) => {
-  if (request.method !== 'POST') {
-    return response
-      .status(httpStatus.METHOD_NOT_ALLOWED)
-      .send({
-        code: httpStatus.METHOD_NOT_ALLOWED,
-        message: httpStatus.getStatusText(httpStatus.METHOD_NOT_ALLOWED)
-      })
-  }
-  const result = util.validateEmail(request.body)
-  if (result.error) {
-    return response
-      .status(httpStatus.BAD_REQUEST)
-      .send({
-        code: httpStatus.BAD_REQUEST,
-        message: httpStatus.getStatusText(httpStatus.BAD_REQUEST),
-        details:
-          result.error.details &&
-          result.error.details.map(err => {
-            return {
-              message: err.message,
-              param: err.path.join('.')
-            }
-          })
-      })
-  }
-  const { to, subject, text, from } = request.body
-  const mailOptions = mailer.template(from, to, subject, text)
-  mailTransport
-    .sendMail(mailOptions)
-    .then(data =>
-      response.send({
-        data: {
-          success: true,
-          message: 'Email Sent'
-        }
-      })
-    )
-    .catch(err => res
-      .status(httpStatus.INTERNAL_SERVER_ERROR)
-      .send({
-        code: httpStatus.INTERNAL_SERVER_ERROR,
-        message: httpStatus.getStatusText(httpStatus.INTERNAL_SERVER_ERROR),
-        details: err.message
-      })
-    )
-});
+exports.api = functions.https.onRequest(app)
