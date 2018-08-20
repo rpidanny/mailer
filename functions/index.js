cbabbc3b429ea1d7ba3b34bab3611d5f34f5674b
rpidanny <abhishekmaharjan1993@gmail.com>
2018-08-20 16:15:48 +0200

    Add mail block

--- functions/index.js
+++ functions/index.js
@@ -46,6 +46,26 @@ app.post('/mail', (req, res) => {
           })
       })
   }
+  const { to, subject, text, from } = req.body
+  const mailOptions = mailer.template(from, to, subject, text)
+  mailTransport
+    .sendMail(mailOptions)
+    .then(data =>
+      res.send({
+        data: {
+          success: true,
+          message: 'Email Sent'
+        }
+      })
+    )
+    .catch(err => res
+      .status(httpStatus.INTERNAL_SERVER_ERROR)
+      .send({
+        code: httpStatus.INTERNAL_SERVER_ERROR,
+        message: httpStatus.getStatusText(httpStatus.INTERNAL_SERVER_ERROR),
+        details: err.message
+      })
+    )
 })
 
 exports.hello = functions.https.onRequest((request, response) => {
